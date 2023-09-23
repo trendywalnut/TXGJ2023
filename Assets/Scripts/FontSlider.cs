ec3eb5d2d2f2219a7fc0480b3569c8b847699c81
Ali Kedwaii <ali.kedwaii@utexas.edu>
2023-09-23 14:44:09 -0500

    Adding font size slider

---
+++ Assets/Scripts/FontSlider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FontSlider : MonoBehaviour
+{
+    [SerializeField] private Slider ValueSlider;
+    [SerializeField] private TMPro.TMP_Text Title;
+    [SerializeField] private TMPro.TMP_Text FontValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ValueSlider.onValueChanged.AddListener(delegate { OnSliderValueChanged(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSliderValueChanged()
+    {
+        var newFontSize = ValueSlider.value;
+        Title.fontSize = newFontSize;
+        FontValue.fontSize = newFontSize;
+        FontValue.text = newFontSize.ToString();
+    }
+}
