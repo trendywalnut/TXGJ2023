57c55396c4a59e5ded8c5a30133a2cd6acaabc22
meastman555 <masoneastman555@gmail.com>
2023-09-23 21:22:46 -0500

    MinigameManager now uses scene build index order

--- Assets/Scripts/MinigameManager.cs
+++ Assets/Scripts/MinigameManager.cs
@@ -5,11 +5,6 @@ public class MinigameManager : MonoBehaviour
 {
     public static MinigameManager Instance;
 
-    [SerializeField] 
-    [Tooltip("Order minigames will be played in. Elements are the Build Index of each scene. Will advance automatically by calling MoveToNextScene() from a minigame.")]
-    private int[] MinigameScenesIndices;
-    private int CurrentSceneIndex = -1;
-
     private AudioSource Audio;
 
     private void Awake()
@@ -34,11 +29,6 @@ public class MinigameManager : MonoBehaviour
     // Called from each mini game to advance to the next
     public void MoveToNextScene()
     {
-        CurrentSceneIndex++;
-        if(CurrentSceneIndex >= 0 && CurrentSceneIndex < MinigameScenesIndices.Length)
-        {
-            Debug.Log("Loading Scene " + MinigameScenesIndices[CurrentSceneIndex]);
-            SceneManager.LoadScene(MinigameScenesIndices[CurrentSceneIndex]);
-        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
